964917d74ab5f88fbd35bc2e74eff82c8de29763
Abhinav RM <abhinavrm26@gmail.com>
2026-01-07 22:21:36 +0530

    Add files via upload

---
+++ Python-project.py
@@ -0,0 +1,9 @@
+a = float(input("Enter first number : "))
+b = float(input("Enter second number : "))
+
+if a > b:
+    print(f"{a} is bigger 👑🔥 ")
+elif b > a:
+    print(f"{b} is bigger 🚀😤 ")
+else:
+    print("huh, they're equal ⚖️")
